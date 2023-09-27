1aa03d865bf57fee8b9d0103f94227bcdd5d5fd6
Gregory Pappas <gnpappas@proton.me>
2023-09-26 14:53:08 -1000

    Basic idea complete. Change channel needs to be implemented.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('test');
+const gif = document.querySelector('#station');
+
+fetch(
+    "https://api.giphy.com/v1/gifs/random?api_key=msOvtkTOZjU8s7HA4BfHaOxZ2cTejHYm&tag=vaporwave&rating=g"
+)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data.data.images.downsized.url);
+        gif.style.backgroundImage = `url(${data.data.images.downsized.url})`;
+    });
