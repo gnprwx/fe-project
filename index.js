a756b74b6d98d1863de29c7397126909ecd5e9e1
Gregory Pappas <gnpappas@proton.me>
2023-09-26 18:04:42 -1000

    Basic terminal functionality with tv

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const gif = document.querySelector('#station');
+const gif = document.querySelector("#station");
+const terminal = document.querySelector("#terminal");
 
-fetch(
-    "https://api.giphy.com/v1/gifs/random?api_key=msOvtkTOZjU8s7HA4BfHaOxZ2cTejHYm&tag=vaporwave&rating=g"
-)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data.data.images.downsized.url);
-        gif.style.backgroundImage = `url(${data.data.images.downsized.url})`;
-    });
+let tvStatus = false;
+
+terminal.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        let userInput = terminal.value.toLowerCase();
+        if (userInput === "on" && tvStatus) {
+            setTimeout(() => {
+                terminal.placeholder = "";
+            }, 2000);
+            terminal.placeholder = "TV is already on, dude.";
+        } else if (userInput === "on" && !tvStatus) {
+            terminal.placeholder = "'change' the channels";
+            tvStatus = true;
+            tvChannels();
+        }
+        if (userInput === "change") {
+            terminal.placeholder = "";
+            tvChannels();
+        }
+        if (userInput === "off") {
+            tvStatus = false;
+            gif.style.backgroundImage = `none`;
+            terminal.placeholder = "TV is turned off.";
+        }
+        terminal.value = '';
+    }
+});
+
+function tvChannels() {
+    fetch(
+        "https://api.giphy.com/v1/gifs/random?api_key=msOvtkTOZjU8s7HA4BfHaOxZ2cTejHYm&tag=vaporwave&rating=g"
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            gif.style.backgroundImage = `url(${data.data.images.downsized.url})`;
+        });
+}
