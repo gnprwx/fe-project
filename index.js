ffd9b45bffc59b7bebd6c0c19cb4b91dc87991e3
Gregory Pappas <gnpappas@proton.me>
2023-09-27 09:15:36 -1000

    Add weather command w/ widget command.

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 const gif = document.querySelector("#station");
 const terminal = document.querySelector("#terminal");
-const GKEY = 'msOvtkTOZjU8s7HA4BfHaOxZ2cTejHYm';
+const icon = document.createElement('img');
+icon.className = 'wIcon';
+gif.appendChild(icon);
+
+const GKEY = "msOvtkTOZjU8s7HA4BfHaOxZ2cTejHYm";
+const WKEY = "eafc929406722a97ee0c5f953c3bdf13";
+
 let tvStatus = false;
 
 terminal.addEventListener("keydown", (event) => {
-    //! MOVE COMMANDS TO SEPARATE FUNCTION
     if (event.key === "Enter") {
-        let userInput = terminal.value.toLowerCase();
-        if (userInput === "on" && tvStatus) {
-            setTimeout(() => {
-                terminal.placeholder = "";
-            }, 2000);
-            terminal.placeholder = "TV is already on, dude.";
-        } else if (userInput === "on" && !tvStatus) {
-            terminal.placeholder = "'change' the channels";
-            tvStatus = true;
-            tvChannels();
-        }
-        if (userInput === "change") {
-            terminal.placeholder = "";
-            tvChannels();
-        }
-        if (userInput === "off") {
-            tvStatus = false;
-            gif.style.backgroundImage = `none`;
-            terminal.placeholder = "TV is turned off.";
-        }
-        terminal.value = '';
+        commandRouting();
     }
 });
+
+function commandRouting() {
+    let userInput = terminal.value.toLowerCase();
+    if (userInput === "on" && tvStatus) {
+        setTimeout(() => {
+            terminal.placeholder = "";
+        }, 2000);
+        terminal.placeholder = "TV is already on, dude.";
+    } else if (userInput === "on" && !tvStatus) {
+        terminal.placeholder = "'change' the channels";
+        tvStatus = true;
+        tvChannels();
+    }
+    if (userInput === "change" && tvStatus) {
+        terminal.placeholder = "";
+        tvChannels();
+    }
+    if (userInput === "off") {
+        tvStatus = false;
+        gif.style.backgroundImage = `none`;
+        terminal.placeholder = "TV is turned off.";
+    }
+    if (userInput === "weather") {
+        currentWeather();
+    }
+    if (userInput === 'widget') {
+        terminal.placeholder = "";
+        icon.classList.toggle('widget');
+        icon.classList.toggle('wIcon');
+    }
+    terminal.value = "";
+}
 
 function tvChannels() {
     fetch(
@@ -39,5 +56,18 @@ function tvChannels() {
         })
         .then((data) => {
             gif.style.backgroundImage = `url(${data.data.images.downsized.url})`;
+        });
+}
+
+function currentWeather() {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=21.30&lon=-157.85&appid=${WKEY}&units=imperial`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            icon.src = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+            terminal.placeholder = `Weather in paradise is currently ${Math.round(data.main.temp)}°F with ${data.weather[0].description}.`
         });
 }
