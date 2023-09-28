4e6645b585ac416cc1a2ad5fa37560bb9719f926
Gregory Pappas <gnpappas@proton.me>
2023-09-28 09:00:40 -1000

    Refactor commands into individual functions. Route commands via switch conditions for code clarity

--- index.js
+++ index.js
@@ -10,91 +10,107 @@ temp.className = "tIcon";
 station.appendChild(icon);
 station.appendChild(temp);
 
-terminal.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        commandRouting();
-    }
-    return;
-});
+const commands = [
+    "on",
+    "off",
+    "change",
+    "screen",
+    "weather",
+    "widget",
+    "tldr",
+    "clear",
+];
 
 let tvStatus = false;
 let weather = false;
 let full = false;
 
-function commandRouting() {
-    const commands = [
-        "on",
-        "off",
-        "change",
-        "screen",
-        "weather",
-        "widget",
-        "tldr",
-        "clear"
-    ];
+//? command routing
+terminal.addEventListener("keydown", (event) => {
     let userInput = terminal.value.toLowerCase();
-    if (userInput === commands[0]) {
-        if (tvStatus) {
-            terminal.value = "";
-            setTimeout(() => {
+    if (commands.includes(userInput) && event.key === "Enter") {
+        switch (userInput) {
+            case commands[0]:
+                tvOn();
+                break;
+            case commands[1]:
+                tvOff();
+                break;
+            case commands[2]:
+                tvStatus ? tvChannels() : null;
+                break;
+            case commands[3]:
+                tvStatus ? screenSize() : null;
+                break;
+            case commands[4]:
+                currentWeather();
+                weather = true;
+                break;
+            case commands[5]:
+                weatherWidget();
+                break;
+            case commands[6]:
+                terminal.placeholder = `commands: ${commands.join(" | ")}`;
+                break;
+            case commands[7]:
                 terminal.placeholder = "";
-            }, 2000);
-            terminal.placeholder = "TV is already on, dude.";
-            return;
-        } else {
-            terminal.placeholder = "'change' the channels";
-            tvStatus = true;
-            tvChannels();
+                break;
         }
-    } 
-    if (userInput === commands[1]) {
-        tvStatus = false;
-        station.style.backgroundImage = `none`;
-        terminal.placeholder = "TV is turned off.";
-    } 
-    if (tvStatus) {
-        if (userInput === commands[2]) {
-            tvChannels();
-        } else if (userInput === commands[3]) {
-            if (!full) {
-                tv.style.backgroundImage = "none";
-                station.style.backgroundSize = "cover";
-                full = true;
-            } else {
-                tv.style.backgroundImage = "url('./img/tv.png')";
-                station.style.backgroundSize = "contain";
-                full = false;
-            }
-        }
-    } 
-    if (userInput === commands[4]) {
-        currentWeather();
-        weather = true;
-    } 
-    if (userInput === commands[5]) {
-        if (weather) {
-            temp.classList.toggle("temp");
-            temp.classList.toggle("tIcon");
-            icon.classList.toggle("widget");
-            icon.classList.toggle("wIcon");
-        } else {
-            terminal.value = "";
-            setTimeout(() => {
-                terminal.placeholder = "";
-            }, 2000);
-            terminal.placeholder = "Must run 'weather' at least once first.";
-        }
+        terminal.value = "";
     }
-    if (userInput === commands[6]) {
-        terminal.placeholder = `commands: ${commands.join(" | ")}`;
-    }
-    if (userInput === commands[7]) {
-        terminal.placeholder = "";
-    }
-    terminal.value = "";
     return;
+});
+
+//? command functions
+function tvOn() {
+    if (tvStatus) {
+        terminal.value = "";
+        setTimeout(() => {
+            terminal.placeholder = "";
+        }, 2000);
+        terminal.placeholder = "TV is already on, dude.";
+        return;
+    } else {
+        terminal.placeholder = "'change' the channels";
+        tvStatus = true;
+        tvChannels();
+    }
 }
 
+function tvOff() {
+    tvStatus = false;
+    station.style.backgroundImage = `none`;
+    terminal.placeholder = "TV is turned off.";
+}
+
+function screenSize() {
+    if (!full) {
+        tv.style.backgroundImage = "none";
+        station.style.backgroundSize = "cover";
+        full = true;
+    } else {
+        tv.style.backgroundImage = "url('./img/tv.png')";
+        station.style.backgroundSize = "contain";
+        full = false;
+    }
+}
+
+function weatherWidget() {
+    if (weather) {
+        temp.classList.toggle("temp");
+        temp.classList.toggle("tIcon");
+        icon.classList.toggle("widget");
+        icon.classList.toggle("wIcon");
+    } else {
+        terminal.value = "";
+        setTimeout(() => {
+            terminal.placeholder = "";
+        }, 2000);
+        terminal.placeholder = "Must run 'weather' at least once first.";
+    }
+}
+
+//? API CALLS
 function tvChannels() {
     const GKEY = "msOvtkTOZjU8s7HA4BfHaOxZ2cTejHYm";
     fetch(
